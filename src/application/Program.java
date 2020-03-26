3e154b62218b13d7ad270b545f7ad66a5b89502c
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2020-03-26 11:40:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
 
-        System.out.println(department);
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(1, "Ryan Sel", "ryan@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
